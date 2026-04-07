716ae6ecac9cde819169fc9c8e8450111b986611
wgdcwd <86083157+wgdcwd@users.noreply.github.com>
2026-04-07 22:02:05 +0900

    미니게임 base코드, JumpGame, BallPopGame 생성

---
+++ Assets/01.Scripts/Minigame/BallPopGame/BallPopGame.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BallPopGame : MiniGame
+{
+    public GameObject[] balls;
+
+    int _remaining;
+
+    protected override void OnStart()
+    {
+        foreach (GameObject ball in balls)
+            ball.SetActive(true);
+
+        _remaining = balls.Length;
+    }
+
+    void Update()
+    {
+        if (!Mouse.current.leftButton.wasPressedThisFrame) return;
+
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        Collider2D hit = Physics2D.OverlapPoint(worldPos);
+
+        if (hit == null) return;
+
+        for (int i = 0; i < balls.Length; i++)
+        {
+            if (hit.gameObject != balls[i]) continue;
+
+            balls[i].SetActive(false);
+            _remaining--;
+
+            if (_remaining <= 0) Clear();
+            return;
+        }
+    }
+}
---
+++ Assets/01.Scripts/Minigame/MiniGame.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System;
+
+public abstract class MiniGame : MonoBehaviour
+{
+    public static event Action<MiniGame> OnCleared;
+
+    public void StartGame()
+    {
+        gameObject.SetActive(true);
+        OnStart();
+    }
+
+    protected void Clear()
+    {
+        Debug.Log($"[MiniGame] {gameObject.name} CLEAR");
+        OnCleared?.Invoke(this);
+        gameObject.SetActive(false);
+    }
+
+    protected abstract void OnStart();
+}
