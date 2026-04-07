dd3eb41dd562197a01d68b4964d2f1d8ed98b179
thlee99813@gmail.com <thlee99813@gmail.com>
2026-04-07 21:51:05 +0900

    [이태환] 테스트 미니게임 구현

---
+++ Assets/01.Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    protected override void Init()
+    {
+        
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+}
+
+
