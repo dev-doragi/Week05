44bceddfb1d4a96daa337e3a5923753a81e3eca8
Choijaein0125 <Choijaein0125@gmail.com>
2026-04-07 17:07:27 +0900

    Project Setting
    규칙 및 컨벤션은 Readme 참조

---
+++ Assets/01.Scripts/Frame/EventArgs.cs
@@ -0,0 +1,23 @@
+﻿//using System;
+//using UnityEngine;
+
+//[Serializable]
+//public class StageActivatedEventArgs : EventArgs
+//{
+//    public StageContext StageContext;
+//    public StageActivatedEventArgs(StageContext stageContext)
+//    {
+//        StageContext = stageContext;
+//    }
+//}
+//[Serializable]
+//public class BigTurnStartedEventArgs : EventArgs
+//{
+//    public int BigTurn;
+//    public BigTurnStartedEventArgs(int bigTurn)
+//    {
+//        BigTurn = bigTurn;
+//    }
+//}
+
+
