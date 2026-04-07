76d55c4d9ce735c90f4fb74b5176fac7659bf296
이하녕 <85740652+fbdfbd@users.noreply.github.com>
2026-04-07 22:02:18 +0900

    feat. SO Data 추가

---
+++ Assets/01.Scripts/UI/SO/SO_UIComponentImageData.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SO_UIComponentImageData", menuName = "Scriptable Objects/SO_UIComponentImageData")]
+public class SO_UIComponentImageData : ScriptableObject
+{
+    
+}
