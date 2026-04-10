7a5e241493d3f40857625112483287f10aca4128
wgdcwd <86083157+wgdcwd@users.noreply.github.com>
2026-04-10 19:39:25 +0900

    Stage 스크립트, MisionClearer 추가

---
+++ Assets/01.Scripts/InGame/MissionClearer.cs
@@ -0,0 +1,10 @@
+﻿// MissionClearer.cs
+using UnityEngine;
+
+public class MissionClearer : MonoBehaviour
+{
+    [SerializeField] private Stage stage;
+    [SerializeField] private int missionIndexToClear;
+
+    public void ClearMission() => stage.ClearMission(missionIndexToClear);
+}
---
+++ Assets/01.Scripts/InGame/Stage.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using UnityEngine;
+
+public abstract class Stage : MonoBehaviour
+{
+    public static event Action<Stage> OnStageChanged;
+
+    [SerializeField] private StageDefinition stageSO;
+    public StageDefinition StageSO => stageSO;
+
+    private StageMission[] _runtimeMissions;
+    public StageMission[] RuntimeMissions => _runtimeMissions;
+
+    private void Awake()
+    {
+        _runtimeMissions = (StageMission[])stageSO.Missions.Clone();
+    }
+
+    public void StartGame()
+    {
+        Debug.Log(StageSO.StageId);
+        gameObject.SetActive(true);
+        OnStart();
+    }
+
+    protected void Clear()
+    {
+        OnStageChanged?.Invoke(this);
+        gameObject.SetActive(false);
+    }
+
+    protected abstract void OnStart();
+
+    public void ClearMission(int index)
+    {
+        Debug.Log($"Clear Mission {index}");
+        if (index < 0 || index >= _runtimeMissions.Length) return;
+        if (_runtimeMissions[index].isMissionSuccess) return;
+
+        _runtimeMissions[index].isMissionSuccess = true;
+
+        if (IsAllMissionCleared())
+        {
+            Debug.Log("Stage Clear!");
+            Clear();
+            return;
+        }
+
+        OnStageChanged?.Invoke(this);
+    }
+
+    private bool IsAllMissionCleared()
+    {
+        foreach (var m in _runtimeMissions)
+            if (!m.isMissionSuccess) return false;
+        return true;
+    }
+}
