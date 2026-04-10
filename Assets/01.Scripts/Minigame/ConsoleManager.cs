5d20817832b39638322eeeacde5ad58cc3ba3ea3
wgdcwd <86083157+wgdcwd@users.noreply.github.com>
2026-04-10 16:42:41 +0900

    Stage SO, Controller, ConsoleManager

---
+++ Assets/01.Scripts/Minigame/ConsoleManager.cs
@@ -0,0 +1,38 @@
+﻿// ConsoleManager.cs
+using UnityEngine;
+using System.Collections.Generic;
+
+public class ConsoleManager : MonoBehaviour
+{
+    // 씬에 있는 StageController들을 에디터에서 등록하거나 자동수집
+    [SerializeField] private List<StageController> stageControllers;
+
+    void Awake()
+    {
+        // 에디터에서 안 꽂아줬으면 씬에서 자동수집
+        if (stageControllers == null || stageControllers.Count == 0)
+            stageControllers = new List<StageController>(FindObjectsByType<StageController>(FindObjectsSortMode.None));
+    }
+
+    void OnEnable()
+    {
+        StageController.OnStageChanged += HandleStageChanged;
+    }
+
+    void OnDisable()
+    {
+        StageController.OnStageChanged -= HandleStageChanged;
+    }
+
+    private void HandleStageChanged(StageController stage)
+    {
+        if (stage.IsStageClear)
+        {
+            // TODO: 스테이지 클리어 UI
+        }
+        else
+        {
+            // TODO: 미션 진행 UI (stage.Missions 순회해서 각 미션 상태 반영)
+        }
+    }
+}
---
+++ Assets/01.Scripts/Minigame/MissionClearTest.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class MissionClearTest : MonoBehaviour
+{
+
+    public StageController stage;
+    public bool isMissionClear;
+    public int missionIndex = 0;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isMissionClear)
+        {
+            stage.ClearMission(missionIndex);
+        }
+    }
+}
