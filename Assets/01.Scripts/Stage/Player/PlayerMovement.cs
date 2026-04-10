a9914f1b68fa9c8865fce7975078f4e72d75581f
wgdcwd <86083157+wgdcwd@users.noreply.github.com>
2026-04-11 00:00:26 +0900

    Update PlayerMovement.cs

--- Assets/01.Scripts/Stage/Player/PlayerMovement.cs
+++ Assets/01.Scripts/Stage/Player/PlayerMovement.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
@@ -17,7 +17,10 @@ public class PlayerMovement : MonoBehaviour
     private void Awake()
     {
         _rigid = GetComponent<Rigidbody2D>();
+    }
 
+    private void OnEnable()
+    {
         StageMove.OnMove += HandleMove;
     }
 
