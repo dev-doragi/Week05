e210ab11b1d298addd5cb94c043b983eb64fbece
이하녕 <85740652+fbdfbd@users.noreply.github.com>
2026-04-11 11:44:08 +0900

    feat. 이름변경 및 플레이어 닿으면 리스폰 추가

--- Assets/01.Scripts/InGame/Debugging/RGB Debugging/RGBObject.cs
+++ Assets/01.Scripts/InGame/Debugging/RGB Debugging/RGBObject.cs
@@ -5,8 +5,12 @@ using UnityEngine.UI;
 public class RGBObject : MonoBehaviour
 {
     [SerializeField] private RGBPanel _panel;
-    [SerializeField] private int _objectID;
+   
+    private int _objectID;
+    private Transform _spawnPos;
+
     [SerializeField] private SpriteRenderer _targetRect;
+    [SerializeField] private Collider2D _collider;
 
     [SerializeField] private Slider _sliderR;
     [SerializeField] private Slider _sliderG;
@@ -14,13 +18,10 @@ public class RGBObject : MonoBehaviour
 
     [SerializeField] private MissionClearer _missionClearer;
 
-    
     [Header("Settings")]
     [Range(0f, 1f)]
     [SerializeField] private float _clearThreshold = 0.1f;
-    // 목표 색상
     [SerializeField] private Color _targetColor;
-    // 초기 색상
     [SerializeField] private Color _defaultColor;
 
     public event Action<int> Success;
@@ -28,43 +29,58 @@ public class RGBObject : MonoBehaviour
 
     private bool _isInitialized;
 
-
-    void Awake()
+    private void Awake()
     {
-        
+        if (_missionClearer == null)
+            _missionClearer = GetComponent<MissionClearer>();
+
+        if (_missionClearer == null)
+            _missionClearer = GetComponentInParent<MissionClearer>();
     }
 
-    public void Init(int id)
+    public void Init(int id, Transform spawnPos)
     {
         SetID(id);
+        _spawnPos = spawnPos;
+        IsCleared = false;
 
-        _sliderR.onValueChanged.AddListener(_ => UpdatePlayerColor());
-        _sliderG.onValueChanged.AddListener(_ => UpdatePlayerColor());
-        _sliderB.onValueChanged.AddListener(_ => UpdatePlayerColor());
+        if (_isInitialized == false)
+        {
+            _sliderR.onValueChanged.AddListener(HandleSliderChanged);
+            _sliderG.onValueChanged.AddListener(HandleSliderChanged);
+            _sliderB.onValueChanged.AddListener(HandleSliderChanged);
+            _isInitialized = true;
+        }
 
         _sliderR.SetValueWithoutNotify(_defaultColor.r);
         _sliderG.SetValueWithoutNotify(_defaultColor.g);
         _sliderB.SetValueWithoutNotify(_defaultColor.b);
 
-
         _targetRect.color = _defaultColor;
-
+        _collider.isTrigger = true;
         UpdatePlayerColor();
     }
 
     private void SetID(int id)
     {
         _objectID = id;
-    }   
+    }
 
-    void UpdatePlayerColor()
+    private void HandleSliderChanged(float _)
+    {
+        UpdatePlayerColor();
+    }
+
+    private void UpdatePlayerColor()
     {
         _targetRect.color = new Color(_sliderR.value, _sliderG.value, _sliderB.value);
         SuccessCheck();
     }
 
-    void SuccessCheck()
+    private void SuccessCheck()
     {
+        if (IsCleared) return;
+
         float diff = ColorDifference(_targetRect.color, _targetColor);
 
         Debug.Log($"[RGBMatchGame] 오차: {diff:F3} / 기준: {_clearThreshold:F3}");
@@ -72,15 +88,34 @@ public class RGBObject : MonoBehaviour
         if (diff <= _clearThreshold)
         {
             IsCleared = true;
-            _panel.Panel.gameObject.SetActive(false);
-            _targetRect.color = _targetColor;
-            Success.Invoke(_objectID);
-        }
+            _collider.isTrigger = false;
 
+            if (_panel != null && _panel.Panel != null)
+                _panel.Panel.gameObject.SetActive(false);
+
+            _targetRect.color = _targetColor;
+
+            if (_missionClearer != null)
+                _missionClearer.ClearMission();
+            else
+                Debug.LogWarning($"[RGBObject] MissionClearer is missing on {name}");
+
+            Success?.Invoke(_objectID);
+        }
     }
 
-    float ColorDifference(Color a, Color b)
+    private float ColorDifference(Color a, Color b)
     {
         return (Mathf.Abs(a.r - b.r) + Mathf.Abs(a.g - b.g) + Mathf.Abs(a.b - b.b)) / 3f;
     }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (IsCleared) return;
+        if (_spawnPos == null) return;
+        if (!other.CompareTag("Player")) return;
+            
+        other.transform.position = _spawnPos.position;
+    }
+
 }
--- Assets/01.Scripts/Stage/StageRGB.cs
+++ Assets/01.Scripts/Stage/StageRGB.cs
@@ -1,110 +1,32 @@
-using UnityEngine;
 using System;
 using System.Collections.Generic;
+using UnityEngine;
 
 public class StageRGB : Stage
 {
     [Serializable]
     public class TargetData
     {
-        public bool isCleared;
         public RGBObject target;
     }
 
     [Header("RGB Targets")]
     [SerializeField] private List<TargetData> _targetListData = new List<TargetData>();
-
-    private Dictionary<int, TargetData> _targets = new Dictionary<int, TargetData>();
+    [SerializeField] private Transform _spawnPos;
 
     private void Start()
     {
-        _targets.Clear();
+        OnStart();
 
-        for (int i = 0; i < _targetListData.Count; i++)
-        {
-            var data = _targetListData[i];
-
-            if (data.target != null)
-            {
-                _targets.Add(i, data);
-                data.target.Init(i);
-            }
-        }
-
-        SubscribeEvents();
     }
     protected override void OnStart()
     {
-        _targets.Clear();
-
         for (int i = 0; i < _targetListData.Count; i++)
         {
             var data = _targetListData[i];
 
             if (data.target != null)
-            {
-                _targets.Add(i, data);
-                data.target.Init(i);
-            }
+                data.target.Init(i, _spawnPos);
         }
-
-        SubscribeEvents();
-    }
-
-    private void SubscribeEvents()
-    {
-        foreach (var data in _targets.Values)
-        {
-            if (data.target != null)
-            {
-                data.target.Success += TargetSuccess;
-            }
-        }
-    }
-
-    private void OnDisable()
-    {
-        foreach (var data in _targets.Values)
-        {
-            if (data.target != null)
-            {
-                data.target.Success -= TargetSuccess;
-            }
-        }
-    }
-
-    private void TargetSuccess(int index)
-    {
-        if (_targets.TryGetValue(index, out TargetData solvedData))
-        {
-            // 중복 실행 방지를 위해 성공한 타겟 구독 해제
-            if (solvedData.target != null)
-            {
-                solvedData.target.Success -= TargetSuccess;
-            }
-
-            // 클리어 상태 업데이트
-            solvedData.isCleared = true;
-
-            if (AllTargetsCleared())
-            {
-                StageClear();
-            }
-        }
-    }
-
-    private bool AllTargetsCleared()
-    {
-        foreach (var data in _targets.Values)
-        {
-            if (!data.isCleared) return false;
-        }
-        return true;
-    }
-
-    public void StageClear()
-    {
-        Debug.Log("[Stage RGB] Stage Clear!");
-        Clear();
     }
 }
