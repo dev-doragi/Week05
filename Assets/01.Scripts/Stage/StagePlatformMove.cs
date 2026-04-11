830275f7c54ffb0c7f48a8c74d2d986ce8fea2bb
경근 <honne.kim01@gmail.com>
2026-04-11 11:52:53 +0900

    [Add] Stage3 Platform Move 제작중

---
+++ Assets/01.Scripts/Stage/StagePlatformMove.cs
@@ -0,0 +1,46 @@
+using System;
+using UnityEngine;
+
+public class StagePlatformMove : Stage
+{
+    public static Action<bool> OnPlatformMove;
+
+    [SerializeField] private UI_RuntimeReferenceBlocker _runtimeReferenceBlocker;
+    private MissionClearer _mission_Reference;
+
+
+    private void OnEnable()
+    {
+        _runtimeReferenceBlocker.BlockedChanged += HandlePlatformMove;
+        CollisionReporter.OnEnter2D += HandleCollision;
+    }
+
+
+    private void OnDisable()
+    {
+        _runtimeReferenceBlocker.BlockedChanged -= HandlePlatformMove;
+        CollisionReporter.OnEnter2D -= HandleCollision;
+    }
+
+    protected override void OnStart()
+    {
+
+    }
+
+    private void HandlePlatformMove(bool active)
+    {
+        Debug.Log("[Stage Move] Component Reference " + !active);
+
+        OnPlatformMove?.Invoke(!active);
+        if (active) _mission_Reference.ClearMission();
+    }
+
+    private void HandleCollision(Collider2D collision)
+    {
+        MissionClearer _mission_Clear;
+        if (_mission_Clear = collision.GetComponent<MissionClearer>())
+        {
+            _mission_Clear.ClearMission();
+        }
+    }
+}
