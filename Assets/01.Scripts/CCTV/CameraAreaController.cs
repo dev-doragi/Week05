d661cd7c1252144369c8425986b152c71299d84e
thlee99813@gmail.com <thlee99813@gmail.com>
2026-04-11 10:25:30 +0900

    [이태환] 새로운 맵 연동

--- Assets/01.Scripts/CCTV/CameraAreaController.cs
+++ Assets/01.Scripts/CCTV/CameraAreaController.cs
@@ -1,43 +1,13 @@
-using System.Collections;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.EventSystems;
 
-public class CameraAreaController : MonoBehaviour, IPointerClickHandler
+public class CameraAreaController : MonoBehaviour
 {
-    public enum BlinkState
-    {
-        None,
-        Selected,
-        CoachDetected
-    }
-
     [Header("Room Data")]
     [SerializeField] private RoomID _roomId = RoomID.None;
     [SerializeField] private Texture _roomBackgroundTexture;
     [SerializeField] private Texture _roomBackgroundWithCoachTexture;
 
-    [Header("UI")]
-    [SerializeField] private Image _cameraUiBackground;
-    [SerializeField] private Color _normalColor = Color.black;
-    [SerializeField] private Color _selectedBlinkColor = Color.yellow;
-    [SerializeField] private Color _coachDetectedBlinkColor = Color.red;
-    [SerializeField] private float _blinkInterval = 0.5f;
-
-    [SerializeField] private SO_SFX _cameraClickSfx;
-    [SerializeField] private SFXPlayer _sfxPlayer;
-    [SerializeField] private float _clickVolume = 1f;
-
-    private CameraManager _cameraManager;
-    private Coroutine _blinkCoroutine;
-    private BlinkState _currentBlinkState = BlinkState.None;
-
     public RoomID RoomId => _roomId;
-
-    private void Awake()
-    {
-        _cameraManager = FindAnyObjectByType<CameraManager>();
-    }
 
     public Texture GetBackgroundTexture(bool hasCoach)
     {
@@ -47,74 +17,8 @@ public class CameraAreaController : MonoBehaviour, IPointerClickHandler
         return _roomBackgroundTexture;
     }
 
-    public void OnPointerClick(PointerEventData eventData)
-    {
-        if (_cameraManager == null)
-            return;
-
-        _cameraManager.SelectCamera(this);
-        _sfxPlayer.Play(_cameraClickSfx, _clickVolume);
-    }
-
-    public void SetBlinkState(BlinkState blinkState)
-    {
-        if (_currentBlinkState == blinkState)
-            return;
-
-        _currentBlinkState = blinkState;
-
-        if (_currentBlinkState == BlinkState.None)
-        {
-            StopBlinkingInternal();
-            return;
-        }
-
-        if (_blinkCoroutine == null)
-            _blinkCoroutine = StartCoroutine(BlinkLoop());
-    }
-
-    private void StopBlinkingInternal()
-    {
-        if (_blinkCoroutine != null)
-        {
-            StopCoroutine(_blinkCoroutine);
-            _blinkCoroutine = null;
-        }
-
-        if (_cameraUiBackground != null)
-            _cameraUiBackground.color = _normalColor;
-    }
-
-    private IEnumerator BlinkLoop()
-    {
-        if (_cameraUiBackground == null)
-            yield break;
-
-        bool isBlinkColor = false;
-
-        while (true)
-        {
-            _cameraUiBackground.color = isBlinkColor ? GetBlinkColor() : _normalColor;
-            isBlinkColor = !isBlinkColor;
-            yield return new WaitForSeconds(_blinkInterval);
-        }
-    }
-
-    private Color GetBlinkColor()
-    {
-        return _currentBlinkState switch
-        {
-            BlinkState.Selected => _selectedBlinkColor,
-            BlinkState.CoachDetected => _coachDetectedBlinkColor,
-            _ => _normalColor
-        };
-    }
-
-    private void PlayCameraClickSfx()
-    {
-        if (_sfxPlayer == null || _cameraClickSfx == null)
-            return;
-
-        _sfxPlayer.Play(_cameraClickSfx, _clickVolume);
-    }
+    // 현재 미니맵 클릭 구조에서는 안 쓰지만 CameraManager 호환용으로 유지
+    // public void SetBlinkState(BlinkState blinkState)
+    // {
+    // }
 }
