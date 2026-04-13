fe65b3df3cd77978a57040497f2756504b42d86a
경근 <honne.kim01@gmail.com>
2026-04-13 21:45:15 +0900

    [Fix] 각 오브젝트, 스테이지 수정

--- Assets/01.Scripts/Stage/Stage2_Place/PlaceVisual.cs
+++ Assets/01.Scripts/Stage/Stage2_Place/PlaceVisual.cs
@@ -2,58 +2,102 @@ using UnityEngine;
 
 public class PlaceVisual : MonoBehaviour
 {
+    public enum VisualType
+    {
+        Object,
+        Target
+    }
+
+    [Header("Type Settings")]
+    [SerializeField] private VisualType _type;
+
     [Header("References")]
     [SerializeField] private PlaceObject _placeObject;
-    [SerializeField] private SpriteRenderer _outlineSprite;
+    [SerializeField] private PlaceTarget _placeTarget;
+    [SerializeField] private SpriteRenderer _sprite;
 
-    [Header("Outline Settings")]
+    [Header("Object Settings")]
     [SerializeField] private Material _normalMaterial;
     [SerializeField] private Material _selectedMaterial;
     [SerializeField] private Material _placedMaterial;
 
+    [Header("Target Settings")]
+    [SerializeField] private float _pulseSpeed;
+    [SerializeField] private float _minAlpha;
+    [SerializeField] private float _maxAlpha;
+
 
     private void Awake()
     {
-        if (_placeObject == null) _placeObject = GetComponent<PlaceObject>();
-        if (_outlineSprite == null) _outlineSprite = GetComponent<SpriteRenderer>();
+        if (_sprite == null) _sprite = GetComponent<SpriteRenderer>();
+
+        if (_type == VisualType.Object) if (_placeObject == null) _placeObject = GetComponent<PlaceObject>();
+        else if (_type == VisualType.Target) if (_placeTarget == null) _placeTarget = GetComponent<PlaceTarget>();
     }
 
     private void OnEnable()
     {
-        if (_placeObject != null)
+        if (_placeObject != null && _type == VisualType.Object)
         {
-            _placeObject.OnStateChanged += UpdateVisual;
+            _placeObject.OnStateChanged += UpdateObjectVisual;
         }
     }
 
     private void OnDisable()
     {
-        if (_placeObject != null)
+        if (_placeObject != null && _type == VisualType.Object)
         {
-            _placeObject.OnStateChanged -= UpdateVisual;
+            _placeObject.OnStateChanged -= UpdateObjectVisual;
         }
     }
 
     private void Start()
     {
-        UpdateVisual();
+        if (_type == VisualType.Object) UpdateObjectVisual();
     }
 
-    private void UpdateVisual()
+    private void Update()
+    {
+        if (_type == VisualType.Target) UpdateTargetVisual();
+    }
+
+    private void UpdateObjectVisual()
     {
         if (_placeObject == null) return;
-        if (_outlineSprite == null) return;
+        if (_sprite == null) return;
 
         // 1. Already placed: Hide outline
         if (_placeObject.IsPlaced)
         {
-            _outlineSprite.sharedMaterial = _placedMaterial;
+            _sprite.sharedMaterial = _placedMaterial;
 
             return;
         }
 
         // 2. Not Placed: Show outline
-        _outlineSprite.gameObject.SetActive(true);
-        _outlineSprite.sharedMaterial = _placeObject.IsSelected ? _selectedMaterial : _normalMaterial;
+        _sprite.gameObject.SetActive(true);
+        _sprite.sharedMaterial = _placeObject.IsSelected ? _selectedMaterial : _normalMaterial;
+    }
+
+    private void UpdateTargetVisual()
+    {
+        if (_placeTarget == null || _sprite == null) return;
+
+        if (_placeTarget.IsMatch)   // 1. Already matched: Hide target
+        {
+            Color c = _sprite.color;
+            c.a = 0;
+            _sprite.color = c;
+
+            return;
+        }
+        else                        // 2. Not matched: Pulse alpha
+        {
+            float alpha = Mathf.PingPong(Time.time * _pulseSpeed, _maxAlpha - _minAlpha) + _minAlpha;
+            
+            Color c = _sprite.color;
+            c.a = alpha;
+            _sprite.color = c;
+        }
     }
 }
---
+++ Assets/01.Scripts/Stage/Stage3_PlatformMove/Blocked_Visual.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class Blocked_Visual : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private MovingPlatform _platform;
+    [SerializeField] private SpriteRenderer _sprite;
+
+    [Header("Color Settings")]
+    [SerializeField] private Color _normalColor;
+    [SerializeField] private Color _blockedColor;
+
+    [Header("Blocked Settings")]
+    [SerializeField] private float _pulseSpeed;
+    [SerializeField] private float _minAlpha;
+    [SerializeField] private float _maxAlpha;
+
+    private Color _originColor;
+
+    private void Awake()
+    {
+        if (_platform == null) _platform = GetComponent<MovingPlatform>();
+        if (_sprite == null) _sprite = GetComponent<SpriteRenderer>();
+
+        if (_sprite != null) _originColor = _sprite.color;
+    }
+
+    private void Update()
+    {
+        if (_platform == null || _sprite == null) return;
+
+        
+        if (_platform.canMove)      // 1. Normal State: Original color
+        {
+            _sprite.color = _originColor;
+        }
+        else                        // 2. Blocked State: Pulse alpha
+        {
+            float alpha = Mathf.PingPong(Time.time * _pulseSpeed, _maxAlpha - _minAlpha) + _minAlpha;
+
+            Color c = _blockedColor;
+            c.a = alpha;
+            _sprite.color = c;
+        }
+    }
+}
